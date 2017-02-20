b2351198ed4dc919fe505b07608e7786463b83ae
Joseph Haaga <joseph@Joseph-Dev-MBP-R-275.local>
2017-02-20 10:37:46 -0500

    dir structure change

--- joe/login.py
+++ joe/login.py
@@ -4,21 +4,18 @@ from pynput.keyboard import Key, Listener
 
 t0 = str(time.time());
 user = input('What is your username?');
-# Collect events until released
 if not os.path.exists(user+"/"+t0):
 	os.makedirs(user+"/"+t0);
-raw = open(user+"/"+t0+"/raw", 'w+');
+raw = open("keystrokes/"+user+"/"+t0+"/raw", 'w+');
 
 presses = [];
 releases = [];
 
 def on_press(key):
-	# raw.write(key+" "+"PRESSED "+str(time.time()));
 	print ('{0} pressed'.format(key));
 	presses.append({key: time.time()});
 
 def on_release(key):
-	# raw.write(key+" "+"RELEASED "+str(time.time()));
 	print('{0} release'.format(key));
 	if key == Key.esc:
 		# Stop listener
