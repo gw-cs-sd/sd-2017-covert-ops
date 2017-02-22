af4c15114da0eb90131d811799972d057ee09adf
Joseph Haaga <joseph@Joseph-Dev-MBP-R-275.local>
2017-02-22 11:50:18 -0500

    press to press times calculated

--- joe/generate_GREYC.py
+++ joe/generate_GREYC.py
@@ -11,31 +11,29 @@ import os;
 # Functions
 
 # press to press times
-# def press_to_press():
+def press_to_press(dictionary):
+	results = [];
+	for n in range(0,len(dictionary['presses'])-1):
+		print (n)
+		results.append((dictionary['presses'][n+1][1] - dictionary['presses'][n][1])*1000);
+	return results;
+
 
 # Process
 # - Walk keystrokes/unprocessed
 for dir in os.listdir('keystrokes/unprocessed'):
-    # 'dir' is a particular user
-    if dir!='.DS_Store':
-    	print ("-"+dir);
-    	for k in os.listdir('keystrokes/unprocessed/'+dir):
-	    	if k!='.DS_Store':
-		    	# 'k' is a particular timestamp directory, containing 'raw'
-		    	print ("--"+k);
-		    	with open("keystrokes/unprocessed/"+dir+"/"+k+"/raw") as data_file:    
+	# 'dir' is a particular user
+	if dir!='.DS_Store':
+		print ("-"+dir);
+		for k in os.listdir('keystrokes/unprocessed/'+dir):
+			if k!='.DS_Store':
+				# 'k' is a particular timestamp directory, containing 'raw'
+				print ("--"+k);
+				with open("keystrokes/unprocessed/"+dir+"/"+k+"/raw") as data_file:
 					master = json.load(data_file);
 				password = [n[0].replace("'","") for n in master['presses']];
-		    	# - Apply GREY-C functions to each raw file
+				# - Apply GREY-C functions to each raw file
+				p_to_p = press_to_press(master);
+				print (p_to_p);
 				# - Save results in 4 files: pp, pr, rp, and rr
 				# - rename directory into the "processed" directory
-
-
-# with open('raw') as data_file:    
-#     master = json.load(data_file);
-
-# password = ([list(n.keys())[0].replace("'","") for n in master['presses']]);
-
-# print (master['presses']);
-# print ();
-# print (master['releases']);
