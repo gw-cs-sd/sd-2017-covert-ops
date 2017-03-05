bf13b58c339f96d51769ff4ed2f794373548c591
Joseph Haaga <joseph@Joseph-Dev-MBP-R-275.local>
2017-03-05 17:58:12 -0500

    is foundation working?

--- joe/web/generate_config.js
+++ joe/web/generate_config.js
@@ -1,1 +1,11 @@
-// 
+
+
+// http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+function objectifyForm(formArray) {//serialize data function
+
+  returnArray = {};
+  for (var i = 0; i < formArray.length; i++){
+    returnArray[formArray[i]['name']] = formArray[i]['value'];
+  }
+  return returnArray;
+}
