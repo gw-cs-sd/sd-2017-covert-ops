afc069047bde7e255b07024e4ff8c70d2fb5e261
Joseph Haaga <joseph@Joseph-Dev-MBP-R-275.local>
2017-03-05 18:54:41 -0500

    signature recognition slider logic

--- joe/web/generate_config.js
+++ joe/web/generate_config.js
@@ -3,13 +3,18 @@ values = {};
 // Keystroke Detection
 // jQuery('.slider.keystroke_dynamics input').value();
 values['keystroke_dynamics'] = {};
+values['signature_recognition'] = {};
 
 jQuery('.slider.keystroke_dynamics').on('moved.zf.slider',function(){
-	new_kb_val = jQuery(this).find("input").first().attr("value");
+	new_kb_val = jQuery('.slider.keystroke_dynamics').find("input").first().attr("value");
 	values['keystroke_dynamics']['accuracy'] = new_kb_val;
 	update_config();
 });
-
+jQuery('.slider.signature_recognition').on('moved.zf.slider',function(){
+	new_sig_val = jQuery('.slider.signature_recognition').find("input").first().attr("value");
+	values['signature_recognition']['accuracy'] = new_sig_val;
+	update_config();
+});
 
 function update_config(){
 	jQuery('#config').html(JSON.stringify(values));
